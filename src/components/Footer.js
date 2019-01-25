51b1a0ea7c4ecc520a9cfc9b545b5fa6c4a7009f
swindl3 <swindlemaxxx@gmail.com>
2019-01-26 04:23:09 +0700

    @fonts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
-import React from 'react' ;
+import React from "react";
 
-const Footer = (props)=>{
-    const {name,uni} = props
-    return (
-            <div>Powered By :{name+'||'+uni}</div>
-           )
-}
+const Footer = props => {
+  const { name, uni } = props;
+  return (
+    <div className="container-fluid">
+      <hr />
+      <div className="text-center title text-uppercase">
+        <small>
+          <span className="text-danger">Powered By : {name} </span>|
+          <span className="text-muted"> Contact me : {uni}</span>
+        </small>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,51 @@
-import React,{Component} from 'react' ;
+import React, { Component } from "react";
 
 class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state = {date :new Date()}
-        setInterval(()=>{this.tick()},1000)
-    } 
-    componentDidMount(){
-        console.log('componentDidMount');
-        
-    }
-    componentDidUpdate(){
-        console.log('componentDidUpdate');
-    }
-    componentWillUnmount(){
-        console.log('componentWillUnmount');
-    }
-    tick(){
-        // this.state = {date :new Date()} เปลี่ยนข้อมูลใน state แบบนี้ไม่ได้   
-        this.setState({date:new Date()})
-    }
-    render(){
-       return (
-           <div>{this.state.date.toLocaleTimeString()}</div>
-       )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+
+    console.log("constructor");
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+
+    clearInterval(this.timerID);
+  }
+  tick() {
+    // this.state = {date :new Date()} เปลี่ยนข้อมูลใน state แบบนี้ไม่ได้
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    const styles = {height:100,width:100}
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8 text-left">
+            <h1 className="text-danger">
+              <img style={styles} src="/images/logo/logo.png" alt="" /> Hell Cafe
+            </h1>
+          </div>
+          <div className="col-md-4 text-right">
+            <h5 className="text-muted mt-4">{this.state.date.toLocaleTimeString()} </h5>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
-export default Header;  
+export default Header;
